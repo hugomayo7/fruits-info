5839b64d4934846e88697985a26e8961f9dda18e
Hugo Mayonobe <hugomayonobe@gmail.com>
2022-03-29 23:40:23 +0200

    blur background

---
+++ src/components/fruit/FruitCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './FruitCard.module.scss'
+
+export default function FruitCard() {
+  return (
+    <div className={styles.card}>
+        
+    </div>
+  )
+}
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,7 @@ import styles from './Home.module.scss'
 export default function Home() {
   return (
     <div className={styles.home}>
-        <h1>Home page</h1>
+        
     </div>
   )
 }
